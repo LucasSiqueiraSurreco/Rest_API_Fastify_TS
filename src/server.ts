c657f760148ebb3f5f5b0cf3bec880da97bc4076
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-11-06 20:08:07 -0300

    zod

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify'
 import { setupKnex } from './database'
+import { env } from '../env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await setupKnex('sqlite_schema').select('*')
-  return tables
+  const transactions = await setupKnex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
-app.listen({ port: 3333 }, () => console.log('http://localhost:3333'))
+app.listen({ port: env.PORT }, () =>
+  console.log(`http://localhost:${env.PORT}`),
+)
