832734ab06b613499a3038b3d8094f7a960f2191
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-11-07 18:11:14 -0300

    summary and list by id routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,30 @@ import { z } from 'zod'
 import crypto from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await setupKnex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await setupKnex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await setupKnex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.post('/', async (req, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
