7ecf71088d5e5677d83c516eb807ffd0468ce6b3
LucasSiqueiraSurreco <lucassiqueirasurreco@outlook.com>
2023-11-09 09:50:25 -0300

    vitest tests

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -57,45 +57,36 @@ export async function transactionsRoutes(app: FastifyInstance) {
         .where('session_id', sessionId)
         .sum('amount', { as: 'amount' })
         .first()
-
       return { summary }
     },
   )
 
-  app.post(
-    '/',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (req, reply) => {
-      const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit']),
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    let sessionId = req.cookies.sessionId
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       })
+    }
 
-      const { title, amount, type } = createTransactionBodySchema.parse(
-        req.body,
-      )
+    await setupKnex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
 
-      let sessionId = req.cookies.sessionId
-      if (!sessionId) {
-        sessionId = crypto.randomUUID()
-
-        reply.cookie('sessionId', sessionId, {
-          path: '/',
-          maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-        })
-      }
-
-      await setupKnex('transactions').insert({
-        id: crypto.randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * -1,
-        session_id: sessionId,
-      })
-
-      return reply.status(201).send()
-    },
-  )
+    return reply.status(201).send()
+  })
 }
